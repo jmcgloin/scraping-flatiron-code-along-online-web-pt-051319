bba970e7f57a98a1a5521e3d84070881535ef1aa
Jason McGloin <jason.mcgloin@gmail.com>
2019-06-11 23:13:30 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,29 @@
+class Course
 
+	attr_accessor :title, :schedule, :description
+
+	@@all = []
+
+
+	def initialize()
+		self.class.all << self
+	end
+
+	def self.create_course_with_info(title, schedule, description)
+		new_course = self.new
+		new_course.title = title
+		new_course.schedule = schedule
+		new_course.description = description
+		self.all << new_course
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.reset_all
+		@@all = []
+	end
+
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    # binding.pry
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |element|
+      title = element.css('h2').text
+      date = element.css('.date').text
+      description = element.css('p').text
+      Course.create_course_with_info(title, date, description)
+    end
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
